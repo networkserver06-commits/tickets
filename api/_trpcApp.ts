05b8540ec81f7277b8a10c2b1a720684688c959a
LEE Tech <networkserver06@gmail.com>
2026-08-31 16:02:37 +0300

    Use explicit ESM imports in Vercel functions

--- api/_trpcApp.ts
+++ api/_trpcApp.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { createContext } from "../server/_core/context";
-import { appRouter } from "../server/routers";
+import { createContext } from "../server/_core/context.js";
+import { appRouter } from "../server/routers.js";
 
 const app = express();
 app.use(
