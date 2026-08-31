e28a841518291a56c7224e4fb07b0e94f644d6fc
LEE Tech <networkserver06@gmail.com>
2026-08-31 16:02:23 +0300

    Use explicit ESM imports in Vercel functions

--- api/admin/logout.ts
+++ api/admin/logout.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { clearAdminCookie } from "../_adminAuth";
+import { clearAdminCookie } from "../_adminAuth.js";
 
 export default function handler(_req: VercelRequest, res: VercelResponse) {
   clearAdminCookie(res);
