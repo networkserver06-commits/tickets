358a8c98bfe49bc2e37fb979c63e844995e1244d
LEE Tech <networkserver06@gmail.com>
2026-08-31 16:02:32 +0300

    Use explicit ESM imports in Vercel functions

--- api/trpc/[trpc].ts
+++ api/trpc/[trpc].ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import app from "../_trpcApp";
+import app from "../_trpcApp.js";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   return app(req, res);
