f911ed728539a2da2e63db3742b0af20a13e784b
LEE Tech <networkserver06@gmail.com>
2026-08-31 16:02:31 +0300

    Use explicit ESM imports in Vercel functions

--- api/oauth/callback.ts
+++ api/oauth/callback.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import app from "../_oauthApp";
+import app from "../_oauthApp.js";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   return app(req, res);
