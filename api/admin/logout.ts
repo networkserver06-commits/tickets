868c3284c085069c3bea01312280d57a3ab73981
LEE Tech <networkserver06@gmail.com>
2026-08-31 15:58:35 +0300

    Fix Vercel ESM API bundling

--- api/admin/logout.ts
+++ api/admin/logout.ts
@@ -1,6 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import app from "../_adminApp";
+import { clearAdminCookie } from "../_adminAuth";
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  return app(req, res);
+export default function handler(_req: VercelRequest, res: VercelResponse) {
+  clearAdminCookie(res);
+  res.status(200).json({ success: true });
 }
