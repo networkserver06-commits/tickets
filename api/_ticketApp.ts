52a57d048eb1da6630611b9bb45c219780b51fb5
LEE Tech <networkserver06@gmail.com>
2026-08-31 15:58:37 +0300

    Fix Vercel ESM API bundling

--- api/_ticketApp.ts
+++ api/_ticketApp.ts
@@ -1,12 +1,11 @@
 import express from "express";
-import { registerAdminRoutes } from "../server/adminAuth";
+import { requireAdmin } from "./_adminAuth";
 import { registerTicketingRoutes } from "../server/ticketing";
 
 const app = express();
 app.use("/api/webhook/paystack", express.raw({ type: "application/json", limit: "2mb" }));
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true }));
-registerAdminRoutes(app);
-registerTicketingRoutes(app);
+registerTicketingRoutes(app, undefined, requireAdmin);
 
 export default app;
