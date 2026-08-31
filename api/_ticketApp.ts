6fac0957e7d601f5dff0f423cad09e384fb39cde
LEE Tech <networkserver06@gmail.com>
2026-08-31 16:02:34 +0300

    Use explicit ESM imports in Vercel functions

--- api/_ticketApp.ts
+++ api/_ticketApp.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import { requireAdmin } from "./_adminAuth";
-import { registerTicketingRoutes } from "../server/ticketing";
+import { requireAdmin } from "./_adminAuth.js";
+import { registerTicketingRoutes } from "../server/ticketing.js";
 
 const app = express();
 app.use("/api/webhook/paystack", express.raw({ type: "application/json", limit: "2mb" }));
