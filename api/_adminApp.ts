21428a747cc205eeec0607ea20a40265816db6c9
LEE Tech <networkserver06@gmail.com>
2026-08-31 16:08:25 +0300

    Remove unused Vercel wrapper

--- api/_adminApp.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { registerAdminRoutes } from "../server/adminAuth";
-
-const app = express();
-app.use(express.json({ limit: "2mb" }));
-registerAdminRoutes(app);
-
-export default app;
